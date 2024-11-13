83563a12c366b2f32f224ffcae0844269e97e722
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-12 22:44:41 -0500

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    for (const value of args) {
+        arr = arr.filter(item => item !== value);
+    }
+    return arr;
 };
 
 // Do not edit below this line
