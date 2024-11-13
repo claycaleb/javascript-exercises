96e4bb3c52a252214a5d4781da9906d13b3fa8ed
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-12 21:23:31 -0500

    Complete third challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    newString = '';
+    for (let i = 1; i <= string.length; i++) {
+        newString += string[string.length - i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
