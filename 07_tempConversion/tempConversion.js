354d9762930919091c4f77e6e0c1ae948656fa65
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-13 21:58:15 -0500

    Complete seventh execise
    Not the most elegant solution, but it works! I see how Math.round works
    better.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temp) {
+  const tempInCelsius = (temp - 32) / 1.8;
+  return Math.round(tempInCelsius);
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const tempInFahrenheit = (temp * 1.8) + 32;
+  return Math.round(tempInFahrenheit);
 };
 
 // Do not edit below this line
