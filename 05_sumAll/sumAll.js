d3ab2f28d252a66b9b27442f78c5de73e6ec5169
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-13 20:54:29 -0500

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    sum = 0;
+    
+    if (!Number.isInteger(first) || first < 0) return 'ERROR';
+    if (!Number.isInteger(second) || second < 0) return 'ERROR';
 
+    if (first < second) {
+        start = first;
+        end = second;
+    } else {
+        start = second;
+        end = first;
+    }
+    
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
