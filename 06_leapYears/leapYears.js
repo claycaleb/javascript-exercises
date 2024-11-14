be42cb2c58dc0b01659e1b04366bc5ebd7248513
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-13 21:25:17 -0500

    Complete sixth challenge
    Copied the variable names but I think my logic makes more sense.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0 && !(year % 400 === 0)) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+//divisible by 4 = true
+// divisible by 100 and not divisible by 400 = false
 
 // Do not edit below this line
 module.exports = leapYears;
