5989f2a13e1089463371b42bcb27dae8e6a67dc2
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-29 09:32:23 -0500

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    const cleanStr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => !/[\s.,!]/.test(char))
+        .join("");
+    const revStr = cleanStr
+        .split("")
+        .reverse()
+        .join("");
 
+    return cleanStr == revStr;
 };
 
 // Do not edit below this line
