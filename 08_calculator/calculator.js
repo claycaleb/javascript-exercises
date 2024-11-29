6696ab7bf9000d4ff9d29c2cc75446ff91829041
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-29 01:11:30 -0500

    Complete calc exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	const arr = [];
+  for(let i = 0; i < b; i++) {
+    arr.push(a);
+  };
+  return multiply(arr);
+  
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	const arr = [];
+  for(let i = 1; i <= n; i++) {
+    arr.push(i);
+  };
+  return multiply(arr);
 };
 
 // Do not edit below this line
