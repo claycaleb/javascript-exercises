9393af3254d6c18e0ed21c415727d1804944397c
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-29 10:24:22 -0500

    Complete fib exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    const arr = [0, 1];
+
+    for (let i = 1; i < n; i++) {
+        arr.push(arr[i] + arr[i-1]);
+    };
+ 
+    return arr[n];
 };
 
 // Do not edit below this line
