215bbbac1b2fdde9da14ba80b71333b49b0824c8
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-30 22:36:20 -0500

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const mapped = people.map(person => {
+        if ('yearOfDeath' in person) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        };
 
+        return person;
+        
+    })
+        
+    mapped.sort((a, b) => b.age - a.age)
+
+    return mapped[0];
 };
 
 // Do not edit below this line
